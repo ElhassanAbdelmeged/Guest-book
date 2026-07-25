e5e5bc6af271667bff14076bdff3b42ac804a6b8
Elhassan Abdelmaged <elhassan.abdelmaged@ad.cyshield>
2026-07-26 00:10:13 +0300

    Fix base path to match repo name

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // The repository name is used as the base path for GitHub Pages.
 export default defineConfig({
   plugins: [react()],
-  base: "/guest-book/",
+  base: "/Guest-book/",
 });
